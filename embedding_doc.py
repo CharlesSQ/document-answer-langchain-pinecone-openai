70bcd3c9c86c5b497eef041a1ef1b8bf5f595c50
CharlesSQ <charlesjsq@gmail.com>
2023-08-26 20:46:23 -0400

    Implementing index creation

--- embedding_doc.py
+++ embedding_doc.py
@@ -14,6 +14,19 @@ pinecone.init(
     environment=PINECONE_API_ENVIRONMENT
 )
 
+# First, check if our index already exists. If it doesn't, we create it
+print('Checking if index exists...')
+if PINECONE_INDEX_NAME not in pinecone.list_indexes():
+    print('Index does not exist, creating index...')
+    # we create a new index
+    pinecone.create_index(
+        name=PINECONE_INDEX_NAME,
+        metric='cosine',
+        # The OpenAI embedding model `text-embedding-ada-002 uses 1536 dimensions`
+        dimension=1536
+    )
+
+print('Loading document...')
 # Load PDF
 loader = UnstructuredPDFLoader(
     'How_Conversational_Business_Can_Help_You_Get_and_Stay_Closer_to_Customers.pdf')
@@ -30,7 +43,10 @@ texts = text_splitter.split_documents(data)
 
 print(f'You have split your document into {len(texts)} smaller documents')
 
-# Create embeddings from your documents
+# Create embeddings and index from your documents
+print('Creating embeddings and index...')
 embeddings = OpenAIEmbeddings(client='')
 docsearch = Pinecone.from_texts(
     [t.page_content for t in texts], embeddings, index_name=PINECONE_INDEX_NAME)
+
+print('Done!')
